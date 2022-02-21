e9d1776690ee6904e0cfaed9f126bd6b954042dd
ShapeShifter11 <shape.one95@gmail.com>
2022-02-21 15:33:56 +0200

    added additional search functionality and updated documentation

--- flightSearch.c
+++ flightSearch.c
@@ -35,7 +35,6 @@ void setup(void), assert_flight(char *from, char*to, int dist);
 int match(char *from, char *to), find(char *from, char *anywhere);
 void push(char *from, char *to, int dist), pop(char *from, char *to, int *dist);
 void isFLight(char *from, char *to), route(char *to);
-void isFlight1(char *from, char *to);
 
 int main(void){
     char from[20], to[20];
@@ -78,9 +77,9 @@ void setup(void){
 /**
  * @brief populates the flight database
  * 
- * @param from 
- * @param to 
- * @param dist 
+ * @param from char type 
+ * @param to char type
+ * @param dist int type
  * 
  * @return void
  */
@@ -96,10 +95,10 @@ void assert_flight(char *from, char*to, int dist){
 }
 
 /**
- * @brief searches the database for a direct flight and returns the distance otherwise returns 0
+ * @brief searches the database for a direct flight and returns the distance, otherwise returns 0
  * 
- * @param from 
- * @param to 
+ * @param from char type
+ * @param to char type
  * 
  * @return int 
  */
@@ -114,12 +113,12 @@ int match(char *from, char *to){
 }
 
 /**
- * @brief given from, finds anywhere
+ * @brief given from, finds anywhere and returns the distance
  * 
- * @param from 
- * @param anywhere 
+ * @param from char type
+ * @param anywhere char type
  * 
- * @return int 
+ * @return int distance
  */
 int find(char *from, char *anywhere){
     find_pos = 0;
@@ -136,11 +135,37 @@ int find(char *from, char *anywhere){
 }
 
 /**
+ * @brief Determins if there is a route between from and to.
+ * 
+ * @param from char type
+ * @param to char type
+ * 
+ * @return void
+ */
+void isFlight(char *from, char *to){
+    int d, dist;
+    char anywhere[20];
+
+    if(d = match(from, to)){
+        push(from, to, d);
+        return;
+    }
+    if(dist = find(from, anywhere)){
+        push(from, to, dist);
+        isFLight(anywhere, to);
+    }
+    else if(tos > 0){
+        pop(from, to, &dist);
+        isFLight(from, to);
+    }
+}
+
+/**
  * @brief pushes on top of stack
  * 
- * @param from 
- * @param to 
- * @param dist 
+ * @param from char type
+ * @param to char type
+ * @param dist int type
  * 
  * @return void
  */
@@ -158,9 +183,9 @@ void push(char *from, char *to, int dist){
 /**
  * @brief pops from top of stack
  * 
- * @param from 
- * @param to 
- * @param dist 
+ * @param from char type
+ * @param to char type
+ * @param dist int type
  * 
  * @return void
  */
@@ -177,7 +202,9 @@ void pop(char *from, char *to, int *dist){
 /**
  * @brief show the route and total distance 
  * 
- * @param to 
+ * @param to char type
+ * 
+ * @return void
  */
 void route(char *to){
     int dist, t;
